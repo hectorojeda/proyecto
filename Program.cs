e881449a7a7f7c18d62c8c3f5cb36453e2a2126d
hector <hectorojeda995@gmail.com>
2019-09-20 11:04:16 -0500

    version 4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EJERCICIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DATOS DE UNA ESFERA");
+            //instancias
+            super superficie = new super();
+            area area = new area();
+            volumen volumen = new volumen();
+            circunferencia circunferencia = new circunferencia();
+            
+            Console.WriteLine("MUESTRA LA SUPERFICIE DE UNA ESFERA");
+            Console.WriteLine("El radio es: {0}", superficie.r);
+            Console.WriteLine("La superficie es: {0}", superficie.su = 4 * Math.PI * Math.Pow(superficie.r,2));
+            Console.WriteLine("El area es: {0}", a.ar = Math.PI * Math.Pow(a.r,2));
+            Console.WriteLine("El volumen es :{0}",volumen.vol=(4/3)*Math.PI*Math.Pow(volumen.r,3));
+            Console.WriteLine("La circunferencia es: {0}", circunferencia.cir = 2 * Math.PI * circunferencia.r);
+        
+        }
+    }
+}
---
+++ circunferencia.cs
@@ -0,0 +1,5 @@
+public class circunferencia
+{
+    public int r;
+    public double cir;
+}
